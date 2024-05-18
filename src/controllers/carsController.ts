845e32d8f0026585361becae614295f98abc98b1
muhammadAjiSolehudin <muhammadajisolehudin@gmail.com>
2024-05-18 19:24:27 +0700

    update cars crud

--- src/controllers/carsController.ts
+++ src/controllers/carsController.ts
@@ -36,3 +36,31 @@ export const createCar = async (req: Request<{}, {}, Car>, res: Response) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+export const deleteCarsById = async (req: Request, res: Response) => {
+  try {
+    const id = +req.params.id;
+    const car = await CarsModel.query().findById(id);
+    if (!car) {
+      return res.status(404).json({ error: 'Car not found' });
+    }
+    await CarsModel.query().deleteById(id); // Menghapus mobil berdasarkan ID 
+    return res.json({ message: 'Car deleted successfully' });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateCarsById = async (req: Request, res: Response) => {
+    try {
+        const id = +req.params.id;
+        const body = req.body;
+        const articles = await CarsModel.query()
+        .findById(id)
+        .patch(body)
+        .returning("*");
+        return res.json(articles);
+    } catch (err: any) {
+        return res.status(500).json({ error: err.message });
+    }
+};
--- src/routes/api/carRouter.ts
+++ src/routes/api/carRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { getCars, createCar, getCarsById } from '../../controllers/carsController';
+import { getCars, createCar, getCarsById, deleteCarsById, updateCarsById } from '../../controllers/carsController';
 
 const router = Router();
 
 router.get('/cars', getCars);
 router.get('/cars/:id', getCarsById)
 router.post('/cars', createCar);
+router.delete('/cars/:id', deleteCarsById);
+router.patch('/cars/:id', updateCarsById);
 
 
 export default router;
